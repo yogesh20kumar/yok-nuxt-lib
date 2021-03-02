d57316fbf1a14501eb143806f3a766c42a773f27
Yogesh Kumar <yok@le34.dk>
2021-03-02 12:43:29 +0530

    Initial Commit

---
+++ components/lib/yok-left-sidebars.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="yok-left-sidebars">Testtttinnnnnn</div>
+</template>
+<script>
+const CONFIG_NAV_WIDTH = 0;
+export default {
+  name: "yok-left-sidebars",
+  props: {
+    sidebarNavItems: {
+      type: Array,
+      default: () => [],
+    },
+    sidebarCommonNavItems: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      drawerType: "basic", // (basic or tabs)
+      configNavWidth: CONFIG_NAV_WIDTH,
+      configDrawer: true,
+      fixed: false,
+      configTab: 0,
+      configMiniVariant: false,
+      configToolList: [],
+    };
+  },
+  computed: {
+    getNavItems() {
+      return (
+        (this.sidebarNavItems.length > 0 &&
+          this.sidebarNavItems[this.configTab].data) ??
+        []
+      );
+    },
+  },
+  watch: {
+    configMiniVariant(isMini) {
+      this.configNavWidth = isMini ? 0 : CONFIG_NAV_WIDTH;
+      this.$emit("changeMiniVariant", isMini);
+    },
+  },
+  methods: {
+    getToolMiniVariantStatus(isMini) {
+      this.configMiniVariant = isMini;
+    },
+  },
+};
+</script>
