e4dd5800e7d09dafe239174d17f14ca8d24d1f83
Yogesh Kumar <yok@le34.dk>
2021-03-02 14:09:14 +0530

    Initial Commit

--- components/lib/index.js
+++ components/lib/index.js
@@ -1,6 +1,2 @@
 export { default as YokGeneralAlert } from "./yok-general-alert.vue";
-export { default as YokLeftSidebar } from "./yok-left-sidebar.vue";
-export { default as YokLeftSidebars } from "./yok-left-sidebars.vue";
-export { default as YokLeftSidebarTool } from "./yok-left-sidebar-tool.vue";
-export { default as YokLeftSidebarToolItems } from "./yok-left-sidebar-tool-items.vue";
-export { default as YokLogin } from "./yok-login.vue";
+export { default as YokLeftSidebar } from "./yok-left-sidebar/yok-left-sidebar.vue";
