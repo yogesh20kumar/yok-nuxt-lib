809eb9e0538b1b7ec0f66f1893eff88dfb1fa974
Yogesh Kumar <yogesh20.kumar@gmail.com>
2021-04-12 18:07:59 +0530

    App bar modification

--- components/lib/yok-app-bar.vue
+++ components/lib/yok-app-bar.vue
@@ -7,15 +7,35 @@
     color="white"
     elevation="1"
     height="70"
+    class="yok-app-bar"
   >
     <v-avatar tile size="70">
-      <img width="70" contain src="~/static/logo.svg" :alt="projectName" />
+      <img
+        width="70"
+        contain
+        src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-light-text.svg"
+        :alt="projectName"
+      />
     </v-avatar>
+    <!-- eslint-disable -->
     <v-toolbar-title
       v-if="projectName"
       class="hidden-xs-only font-weight-medium app-bar ml-8"
-      >{{ projectName }}</v-toolbar-title
-    >
+      v-html="projectName"
+    ></v-toolbar-title
+    ><!-- eslint-enable -->
+    <yok-search-address
+      append-icon="mdi-apps"
+      prepend-inner-icon="mdi-magnify mdi-rotate-90"
+      :make-clear="true"
+      @append-icon-event="searchAppendIconEvent"
+      @address-details="selectedAddress"
+    />
+    <yok-tool-btn
+      v-if="haveToolIcons"
+      :items="toolIconsList"
+      @tool-action="toolAction"
+    ></yok-tool-btn>
     <v-spacer></v-spacer>
     <v-toolbar-items>
       <v-btn
@@ -51,13 +71,15 @@
           </v-btn>
         </template>
         <v-list>
-          <v-list-item v-if="menuList.heading">
+          <v-list-item v-if="avatarMenuList.heading">
             <v-list-item-title>
-              <span class="font-weight-bold"> {{ menuList.heading }} </span>
+              <span class="font-weight-bold">
+                {{ avatarMenuList.heading }}
+              </span>
             </v-list-item-title>
           </v-list-item>
           <v-list-item-group>
-            <v-list-item v-for="(list, i) in menuList.data" :key="i">
+            <v-list-item v-for="(list, i) in avatarMenuList.data" :key="i">
               <v-list-item-title @click.stop="$emit('menu-item-action', list)">
                 <v-icon>{{ list.icon }}</v-icon>
                 {{ list.title }}
@@ -77,9 +99,9 @@
 </template>
 <script>
 export default {
-  name: 'yok-app-bar',
+  // name: 'yok-app-bar',
   props: {
-    logo: {
+    projectName: {
       type: String,
       default: () => '',
     },
@@ -87,13 +109,21 @@ export default {
       type: Boolean,
       default: () => false,
     },
-    projectName: {
-      type: String,
-      default: () => '',
-    },
-    menuList: {
+    avatarMenuList: {
       type: Object,
       default: () => {},
+    },
+    haveToolIcons: {
+      type: Boolean,
+      default: () => false,
+    },
+    toolIconsList: {
+      type: Array,
+      default: () => [],
+    },
+    haveSearchAddressField: {
+      type: Boolean,
+      default: () => false,
     },
   },
   data() {
@@ -102,10 +132,22 @@ export default {
   computed: {
     getShortName() {
       const matches =
-        this.menuList.heading !== undefined && this.menuList.heading !== ''
-          ? this.menuList.heading.match(/\b(\w)/g)
+        this.avatarMenuList.heading !== undefined &&
+        this.avatarMenuList.heading !== ''
+          ? this.avatarMenuList.heading.match(/\b(\w)/g)
           : ['NIA']
       return matches.join('')
+    },
+  },
+  methods: {
+    toolAction(obj) {
+      this.$emit('tool-item-action', obj)
+    },
+    searchAppendIconEvent() {
+      this.$emit('search-append-icon-action')
+    },
+    selectedAddress(obj) {
+      this.$emit('selected-address', obj)
     },
   },
 }
